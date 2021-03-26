88817aecbcae7a401da08687022d284846f40c25
jy411 <jinyungisnot@gmail.com>
2021-03-26 21:02:36 +0800

    Updates

---
+++ precache-manifest.e5b62ff163ad77aee374c31b93c6ca32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b07c60900606a8247ea4cad58a0d4b",
+    "url": "/tech-test-react-catsapp/index.html"
+  },
+  {
+    "revision": "8fe6b896e332a3953ce1",
+    "url": "/tech-test-react-catsapp/static/css/main.9627f07e.chunk.css"
+  },
+  {
+    "revision": "4908d89df9fe11fd5220",
+    "url": "/tech-test-react-catsapp/static/js/2.a6cd1ad7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tech-test-react-catsapp/static/js/2.a6cd1ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe6b896e332a3953ce1",
+    "url": "/tech-test-react-catsapp/static/js/main.aa5baece.chunk.js"
+  },
+  {
+    "revision": "50534316862a377880c4",
+    "url": "/tech-test-react-catsapp/static/js/runtime-main.43343893.js"
+  }
+]);
