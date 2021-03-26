1b340b6a4d5874686516bebfa315a223fc59af04
jy411 <jinyungisnot@gmail.com>
2021-03-26 18:28:46 +0800

    5. Consider using library classnames to handle logic within assigning classes

--- src/app/page/feedPage/Feed.js
+++ src/app/page/feedPage/Feed.js
@@ -9,26 +9,31 @@ import {useSelector} from "react-redux";
 
 import "./feedStyle.css";
 
+import "classnames";
+
 const Feed = () => {
-  const [pageNum, setPageNum] = useState(1);
+  const classNames = require('classnames');
   const [activePage, setActivePage] = useState(1);
+
+  // Redux Selectors
   const catArr = useSelector(state => state.feed.catArr);
   const dataLoaded = useSelector(state => state.feed.dataLoaded);
 
   const pageSelect = (num) => {
-    setPageNum(num);
     setActivePage(num);
   }
 
+  let allCatsBtnClass = classNames({
+    "activeBtn": activePage === 1,
+    "feedBtn": activePage === 2,
+    "feedBtnText": true
+  });
 
-  let btn1, btn2;
-  if (activePage === 1) {
-    btn1 = "activeBtn feedBtnText"
-    btn2 = "feedBtn feedBtnText"
-  } else {
-    btn1 = "feedBtn feedBtnText"
-    btn2 = "activeBtn feedBtnText";
-  }
+  let likedCatsBtnClass = classNames({
+    "feedBtn": activePage === 1,
+    "activeBtn": activePage === 2,
+    "feedBtnText": true
+  });
 
   if (!dataLoaded) {
     return <SplashScreen />
@@ -43,15 +48,15 @@ const Feed = () => {
           </Grid>
           <Grid xs={18} justify="center" alignItems="center">
             <div className="btnGroup">
-              <button className={btn1}
+              <button className={allCatsBtnClass}
                       onClick={() => pageSelect(1)}>All cats</button>
               <Spacer x={1}/>
-              <button className={btn2}
+              <button className={likedCatsBtnClass}
                       onClick={() => pageSelect(2)}>Liked</button>
             </div>
           </Grid>
-          {pageNum === 1 && catArr.map((item) => {return (<CatGrid catData={item}/>)})}
-          {pageNum === 2 && <LikedCats />}
+          {activePage === 1 && catArr.map((item) => {return (<CatGrid catData={item}/>)})}
+          {activePage === 2 && <LikedCats />}
         </Grid.Container>
       )
     }
